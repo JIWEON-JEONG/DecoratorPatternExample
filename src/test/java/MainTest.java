27d0af20700c8fa3ac4775741096eaa41a3639e7
JIWEON-JEONG <dlektl6@naver.com>
2023-02-22 13:43:17 +0900

    test : NotifyViewHandler 정상 작동 테스트

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Proxy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import decorator.Board;
+import decorator.BoardUseCase;
+import decorator.NotifyViewHandler;
+
+public class MainTest {
+
+	Board board = Board.INSTANCE;
+
+	@Test
+	@DisplayName("NotifyViewHandler 정상 작동 테스트")
+	void notifyViewHandlerTest() {
+		//given
+		board.addView();
+		BoardUseCase boardUseCase = (BoardUseCase)Proxy.newProxyInstance(
+			Main.class.getClassLoader(),
+			new Class[] {BoardUseCase.class},
+			new NotifyViewHandler(board)
+		);
+
+		//when
+		String result = boardUseCase.read();
+
+		//then
+		Assertions.assertEquals("게시물을 읽습니다.\n게시글 접근 순서 : 2번째", result);
+	}
+}
